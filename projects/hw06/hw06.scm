d882c8dbe8d7147d480be8e4b833b5b429874eb0
Zhixuan Yang <zhixuanyang@ZhixuandeMacBook-Pro.local>
2021-04-23 02:02:36 +0800

    Finished CS61a Fa20

---
+++ projects/hw06/hw06.scm
@@ -0,0 +1,28 @@
+(define (cddr s)
+  (cdr (cdr s)))
+
+(define (cadr s)
+  (car (cdr s))
+)
+
+(define (caddr s)
+  (car (cdr (cdr s)))
+)
+
+
+(define (sign num)
+  (cond((> num 0) 1)
+        ((= num 0) 0)
+        (else -1))
+)
+
+
+(define (square x) (* x x))
+
+(define (pow x y)
+  (cond ((= y 0) 1)
+        ((even? y) (square (pow x (/ y 2))))
+        (else (* x (square (pow x (/(- y 1) 2)))))
+    )
+)
+
